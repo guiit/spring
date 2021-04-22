d7d2cdd869b9d29df5c2a7f71c4cbd737aafd2d5
LaraFer10 <lara.ferrer001@gmail.com>
2021-04-22 17:31:17 -0300

    crud historico

--- src/main/java/br/ucsal/gestaoHospitalar/controller/HistoricoController.java
+++ src/main/java/br/ucsal/gestaoHospitalar/controller/HistoricoController.java
@@ -13,26 +13,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import br.ucsal.gestaoHospitalar.entity.Historico;
 import br.ucsal.gestaoHospitalar.service.HistoricoService;
+import br.ucsal.gestaoHospitalar.service.MedicacaoService;
+import br.ucsal.gestaoHospitalar.service.MedicamentoService;
+import br.ucsal.gestaoHospitalar.service.ProcedimentoService;
 
 @Controller
-@RequestMapping("/historico")
+@RequestMapping("/historicos")
 public class HistoricoController {
 	@Autowired
 	private HistoricoService service;
+	@Autowired
+	private ProcedimentoService serviceP;
+	@Autowired
+	private MedicacaoService serviceM;
+	@Autowired
+	private MedicamentoService serviceMedc;
+	
 	@GetMapping()
 	public String exibirFormConsultar(Model model) {
 		
 		List<Historico> historicos = service.findAll();
 		model.addAttribute("historicos", historicos);
 		
-		return "historico";
+		
+		return "historicos";
 	}
 	
 	@GetMapping("/inserir")
 	public String exibirFormHistorico(Model model) {
 		
 		model.addAttribute("historico", new Historico());
-		
+		model.addAttribute("procedimentos", serviceP.findAll());
+		model.addAttribute("medicamentos", serviceMedc.findAll());
+		model.addAttribute("medicacoes", serviceM.findAll());
 		return "new_historico";
 	}
 	
@@ -42,7 +55,7 @@ public class HistoricoController {
             return "redirect:/";
         }
 		service.insert(historico);
-		return "redirect:/historico";
+		return "redirect:/historicos";
 	}
 	
 	@GetMapping("/editar/{id}")
@@ -52,7 +65,9 @@ public class HistoricoController {
 		if(historico == null)
 			throw new IllegalArgumentException("Não existe histórico no sistema com este ID: "+id);
 		model.addAttribute("historico", historico);
-
+		model.addAttribute("procedimentos", serviceP.findAll());
+		model.addAttribute("medicamentos", serviceMedc.findAll());
+		model.addAttribute("medicacoes", serviceM.findAll());
 
 
 		return "editar_historico";
@@ -63,17 +78,17 @@ public class HistoricoController {
 			  BindingResult result, Model model) {
 		service.insert(historico);
 		
-		return "redirect:/historico";
+		return "redirect:/historicos";
 	}
 	
-	@GetMapping("/by/deletar/{id}")
+	@GetMapping("/deletar/{id}")
 	public String deletarHistorico(@PathVariable("id") long id, Model model) {
 		Historico historico = service.getHistorico(id);
 		if(historico == null)
 			throw new IllegalArgumentException("Não existe historico no sistema com este ID: "+id);
 		service.delete(historico);
 		
-		return "redirect:/historico";
+		return "redirect:/historicos";
 	}
 	
 	
